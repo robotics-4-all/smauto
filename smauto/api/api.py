8e20c0828cf3232d62fe31d50ab0422ca4000e4e
Konstantinos Panayiotou <klpanagi@gmail.com>
2023-07-24 21:32:59 +0300

    API improvements

--- smauto/api/api.py
+++ smauto/api/api.py
@@ -57,7 +57,7 @@ img{
     """
 
 
-@api.post("/validate/file")
+@api.post("/validate")
 async def validate_file(file: UploadFile = File(...)):
     print(f'Validation for request: file=<{file.filename}>,' + \
           f' descriptor=<{file.file}>')
@@ -76,23 +76,24 @@ async def validate_file(file: UploadFile = File(...)):
     try:
         model = build_model(fpath)
         print('Model validation success!!')
+        resp['message'] = 'Model validation success'
     except Exception as e:
         print('Exception while validating model. Validation failed!!')
         print(e)
         resp['status'] = 404
-        resp['message'] = e
+        resp['message'] = str(e)
     return resp
 
 
-@api.get("/validate/base64")
-async def validate_b64(fenc: str = ''):
-    if len(fenc) == 0:
+@api.post("/validate/b64")
+async def validate_b64(text: str = ''):
+    if len(text) == 0:
         return 404
     resp = {
         'status': 200,
         'message': ''
     }
-    fdec = base64.b64decode(fenc)
+    fdec = base64.b64decode(text)
     u_id = uuid.uuid4().hex[0:8]
     fpath = os.path.join(
         TMP_DIR,
@@ -103,95 +104,97 @@ async def validate_b64(fenc: str = ''):
     try:
         model = build_model(fpath)
         print('Model validation success!!')
+        resp['message'] = 'Model validation success'
     except Exception as e:
         print('Exception while validating model. Validation failed!!')
+        print(e)
         resp['status'] = 404
         resp['message'] = str(e)
     return resp
 
 
-@api.post("/interpret")
-async def interpret(model_file: UploadFile = File(...),
-                    container: str = 'subprocess',
-                    wait: bool = False):
-    print(f'Interpret Request: file=<{model_file.filename}>,' + \
-          f' descriptor=<{model_file.file}>')
-    resp = {
-        'status': 200,
-        'message': ''
-    }
-    fd = model_file.file
-    u_id = uuid.uuid4().hex[0:8]
-    model_path = os.path.join(
-        TMP_DIR,
-        f'model-{u_id}.auto'
-    )
-
-    with open(model_path, 'w') as f:
-        f.write(fd.read().decode('utf8'))
-    try:
-        if container == 'subprocess':
-            pid = run_interpreter(model_path)
-            if wait:
-                pid.wait()
-        else:
-            raise ValueError()
-    except Exception as e:
-        print(e)
-        resp['status'] = 404
-    return resp
-
-
-@api.post("/graph")
-async def generate(model_file: UploadFile = File(...)):
-    print(f'Generate for request: file=<{model_file.filename}>,' + \
-          f' descriptor=<{model_file.file}>')
-    resp = {
-        'status': 200,
-        'message': ''
-    }
-    fd = model_file.file
-    u_id = uuid.uuid4().hex[0:8]
-    model_path = os.path.join(
-        TMP_DIR,
-        f'model-{u_id}.smauto'
-    )
-    tarball_path = os.path.join(
-        TMP_DIR,
-        f'graph-{u_id}.tar.gz'
-    )
-    gen_path = os.path.join(
-        TMP_DIR,
-        f'gen-{u_id}'
-    )
-    if not os.path.exists(gen_path):
-        os.mkdir(gen_path)
-    with open(model_path, 'w') as f:
-        f.write(fd.read().decode('utf8'))
-    try:
-        model = build_model(model_path)
-        # Build entities dictionary in model. Needed for evaluating conditions
-        model.entities_dict = {entity.name: entity for entity in model.entities}
-        for automation in model.automations:
-            automation.build_condition()
-            fpath = generate_automation_graph(
-                automation,
-                dest=os.path.join(gen_path, f"automation_{automation.name}.pu")
-            )
-            pu_to_png_transformation(fpath, gen_path)
-
-        make_tarball(tarball_path, gen_path)
-        shutil.rmtree(gen_path)
-        print(f'Sending tarball {tarball_path}')
-        return FileResponse(tarball_path,
-                            filename=os.path.basename(tarball_path),
-                            media_type='application/x-tar')
-    except Exception as e:
-        print(e)
-        resp['status'] = 404
-        return resp
-        if os.path.exists(gen_path):
-            shutil.rmtree(gen_path)
+# @api.post("/interpret")
+# async def interpret(model_file: UploadFile = File(...),
+#                     container: str = 'subprocess',
+#                     wait: bool = False):
+#     print(f'Interpret Request: file=<{model_file.filename}>,' + \
+#           f' descriptor=<{model_file.file}>')
+#     resp = {
+#         'status': 200,
+#         'message': ''
+#     }
+#     fd = model_file.file
+#     u_id = uuid.uuid4().hex[0:8]
+#     model_path = os.path.join(
+#         TMP_DIR,
+#         f'model-{u_id}.auto'
+#     )
+#
+#     with open(model_path, 'w') as f:
+#         f.write(fd.read().decode('utf8'))
+#     try:
+#         if container == 'subprocess':
+#             pid = run_interpreter(model_path)
+#             if wait:
+#                 pid.wait()
+#         else:
+#             raise ValueError()
+#     except Exception as e:
+#         print(e)
+#         resp['status'] = 404
+#     return resp
+#
+#
+# @api.post("/graph")
+# async def generate(model_file: UploadFile = File(...)):
+#     print(f'Generate for request: file=<{model_file.filename}>,' + \
+#           f' descriptor=<{model_file.file}>')
+#     resp = {
+#         'status': 200,
+#         'message': ''
+#     }
+#     fd = model_file.file
+#     u_id = uuid.uuid4().hex[0:8]
+#     model_path = os.path.join(
+#         TMP_DIR,
+#         f'model-{u_id}.smauto'
+#     )
+#     tarball_path = os.path.join(
+#         TMP_DIR,
+#         f'graph-{u_id}.tar.gz'
+#     )
+#     gen_path = os.path.join(
+#         TMP_DIR,
+#         f'gen-{u_id}'
+#     )
+#     if not os.path.exists(gen_path):
+#         os.mkdir(gen_path)
+#     with open(model_path, 'w') as f:
+#         f.write(fd.read().decode('utf8'))
+#     try:
+#         model = build_model(model_path)
+#         # Build entities dictionary in model. Needed for evaluating conditions
+#         model.entities_dict = {entity.name: entity for entity in model.entities}
+#         for automation in model.automations:
+#             automation.build_condition()
+#             fpath = generate_automation_graph(
+#                 automation,
+#                 dest=os.path.join(gen_path, f"automation_{automation.name}.pu")
+#             )
+#             pu_to_png_transformation(fpath, gen_path)
+#
+#         make_tarball(tarball_path, gen_path)
+#         shutil.rmtree(gen_path)
+#         print(f'Sending tarball {tarball_path}')
+#         return FileResponse(tarball_path,
+#                             filename=os.path.basename(tarball_path),
+#                             media_type='application/x-tar')
+#     except Exception as e:
+#         print(e)
+#         resp['status'] = 404
+#         return resp
+#         if os.path.exists(gen_path):
+#             shutil.rmtree(gen_path)
 
 
 def run_interpreter(model_path: str):
