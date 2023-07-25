3a8a293a5e7af4b4a6f5e8f2b42a144c2cf9d484
Konstantinos Panayiotou <klpanagi@gmail.com>
2023-07-25 20:46:41 +0300

    Working on new value-gen

--- examples/simple_model/window.py
+++ examples/simple_model/window.py
@@ -3,6 +3,12 @@
 import time
 import random
 
+from enum import Enum
+from dataclasses import dataclass
+from os import wait
+import time
+import numpy as np
+
 from commlib.transports.mqtt import ConnectionParameters
 from rich import print, console, pretty
 from commlib.msg import PubSubMessage
@@ -11,6 +17,138 @@ from commlib.node import Node
 
 pretty.install()
 console = console.Console()
+
+
+# Noise definitions
+# ----------------------------------------
+@dataclass
+class NoiseUniform:
+    min: float
+    max: float
+
+
+@dataclass
+class NoiseGaussian:
+    mu: float
+    sigma: float
+
+
+@dataclass
+class NoiseZero:
+    pass
+
+
+class NoiseType(Enum):
+    Uniform = 1
+    Gaussian = 2
+    Zero = 3
+
+
+class Noise:
+    def __init__(self, _type, properties):
+        self.type = _type
+        self.properties = properties
+
+    def generate(self):
+        if self.type == NoiseType.Uniform:
+            return random.uniform(self.properties.min, self.properties.max)
+        elif self.type == NoiseType.Gaussian:
+            return random.gauss(self.properties.mu, self.properties.sigma)
+# ----------------------------------------
+
+# Value generator definitions
+# ----------------------------------------
+
+@dataclass
+class ValueGeneratorProperties:
+    @dataclass
+    class Constant:
+      value: float
+
+    @dataclass
+    class Linear:
+      start: float
+      increase: float # per second
+
+    @dataclass
+    class Gaussian:
+      value: float
+      max_value: float
+      sigma: float # this is time (seconds)
+
+    @dataclass
+    class Uniform:
+      min: float
+      max: float # this is time (seconds)
+
+
+class ValueGeneratorType(Enum):
+    Constant = 1
+    Linear = 2
+    Gaussian = 3
+    Uniform = 4
+    Step = 5
+    Saw = 6
+    Sinus = 7
+    Logarithmic = 8
+    Exponential = 9
+# ----------------------------------------
+
+
+class ValueComponent:
+    def __init__(self, _type, name, properties, noise):
+        self.type = _type
+        self.name = name
+        self.properties = properties
+        self.noise = noise
+
+
+class ValueGenerator:
+    def __init__(self, namespace, name, hz, components, connection):
+        self.namespace = namespace
+        self.name = name
+        self.hz = hz
+        self.components = components
+
+        self.commlib_node = Node(
+            connection_params=connection,
+            debug=True
+        )
+        topic = f"{self.namespace}/{self.name}" if self.namespace != None else f"{self.name}"
+        self.publisher = self.commlib_node.create_publisher(topic=topic)
+
+    def generate(self, minutes = 1):
+        start = time.time()
+        internal_start = start
+        value = None
+        while time.time() - start < minutes * 60.0:
+            msg = {}
+            for c in self.components:
+                if c.type == ValueGeneratorType.Constant:
+                    value = c.properties.value + c.noise.generate()
+                elif c.type == ValueGeneratorType.Linear:
+                    value = c.properties.start + (time.time() - start) * c.properties.increase
+                    value += c.noise.generate()
+                elif c.type == ValueGeneratorType.Gaussian:
+                    if time.time() - internal_start > 8 * c.properties.sigma:
+                        internal_start = time.time()
+                    value = c.properties.value
+                    _norm_exp = -np.power(
+                        time.time() - internal_start - 4 * c.properties.sigma, 2.
+                    ) / (2 * np.power(c.properties.sigma, 2.))
+                    value += np.exp(_norm_exp) * (c.properties.max_value - c.properties.value)
+                    value += c.noise.generate()
+                msg[c.name] = value
+
+            self.publisher.publish(
+                {
+                    'timestamp': time.time(),
+                    'data': msg
+                }
+            )
+            print(f"Publishing {msg}")
+            time.sleep(1.0 / self.hz)
+
 
 class WindowMsg(PubSubMessage):
         state: bool = False
